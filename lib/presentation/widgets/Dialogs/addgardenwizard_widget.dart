ab083fb85d0267f6038649d628a028698d315a64
farid <farid.jaaoura@aviation-civile.gouv.fr>
2024-02-29 18:43:12 +0100

    ok

--- lib/presentation/widgets/Dialogs/addgardenwizard_widget.dart
+++ lib/presentation/widgets/Dialogs/addgardenwizard_widget.dart
@@ -2,44 +2,51 @@ import 'package:flutter/material.dart';
 import '../../../models/models.dart';
 
 class AddGardenWidget extends StatefulWidget {
-  const AddGardenWidget({super.key});
+  const AddGardenWidget({Key? key}) : super(key: key);
 
   @override
   State<AddGardenWidget> createState() => _AddGardenWidgetState();
 }
 
 class _AddGardenWidgetState extends State<AddGardenWidget> {
-  int _currentStep = 0 ;
-  final  _gardenNameController = TextEditingController();
-   final  _cpController = TextEditingController();
+  int _currentStep = 0;
+  final TextEditingController _gardenNameController = TextEditingController();
+  final TextEditingController _cpController = TextEditingController();
   String _gardenName = "";
 
-   final List<Step> _steps = [
-  Step(
-      title: Text("Nom du jardin"),
-      content: TextField(
-       // controller: _gardenNameController,
-        decoration: const InputDecoration(labelText: 'Nom du jardin'),
-        onChanged: null,
+  late List<Step> _steps;
+
+  @override
+  void initState() {
+    super.initState();
+    _steps = [
+      Step(
+        title: Text("Nom du jardin"),
+        content: TextField(
+          controller: _gardenNameController,
+          decoration: const InputDecoration(labelText: 'Nom du jardin'),
+          onChanged: null,
+        ),
       ),
-    ),
-  Step(
-      title: Text("code postal"),
-      content: TextField(
-      //  controller: _gardenNameController,
-        decoration: const InputDecoration(labelText: 'Nom du jardin'),
-        onChanged: null,
+      Step(
+        title: Text("code postal"),
+        content: TextField(
+           controller: _cpController,
+          decoration: const InputDecoration(labelText: 'Code postal'),
+          onChanged: null,
+        ),
       ),
-    ),
-    Step(
-      title: Text("Planches"),
-      content: Text("Contenu")
-    ),
-     Step(
-      title: Text("Résumé"),
-      content: Text("Contenu")
-    ),
-  ];
+      Step(
+        title: Text("Planches"),
+        content: Text("Contenu"),
+      ),
+      Step(
+        title: Text("Résumé"),
+        content: Text("Contenu"),
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,16 +55,16 @@ class _AddGardenWidgetState extends State<AddGardenWidget> {
       ),
       body: Stepper(
         controlsBuilder: (ctx, details) {
-                          return Container();
-                        },
+          return Container();
+        },
         currentStep: _currentStep,
-        
-        onStepTapped: (step){
-          setState( () {
-            _currentStep=step ;
+        onStepTapped: (step) {
+          setState(() {
+            _currentStep = step;
           });
         },
-      steps: _steps),
+        steps: _steps,
+      ),
     );
   }
 }
